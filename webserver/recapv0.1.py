57778d39f7416677d7791f422bbe01eb0447595e
tgilgs <tomin8or@hotmail.co.uk>
2017-07-24 00:39:07 +0100

    ported homepage over to webserver

--- webserver/recapv0.1.py
+++ webserver/recapv0.1.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, session, url_for, redirect, a
 
 import requests
 import os
+import wordcloud as wc
+import re
+from PIL import Image, ImageDraw
+from io import BytesIO
 
 app = Flask(__name__)
 app.secret_key = os.environ['secret_key']
@@ -33,10 +37,16 @@ def listRooms():
     }
     roomData = (requests.get(url, headers=headers)).json()
 
-    roomDict = {}
+    #roomDict = {}
+    roomArr = []
     for item in roomData["items"]:
-        roomDict[item["title"]] = item["id"]
-    return roomDict
+        #roomDict[item["title"]] = item["id"]
+        roomArr.append(item["title"])
+    #print(roomArr[0])
+
+    #only take the first 8 rooms to save space on nav bar
+    roomArr=roomArr[:8]
+    return roomArr
 
 # Required Parameters: - Access Token
 # Output: - A JSON object containing the user's Cisco Spark details
@@ -53,6 +63,33 @@ def getUserProfile():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+
+
+#Generate wordcloud
+#@app.route('/<outputImgPath>')
+#@app.route('/<imgDimensions>')
+#@app.route('/<inputTextPath>')
+def generate_wordcloud(outputImgPath, imgDimensions, inputTextPath):
+    filepath = open(inputTextPath).read()
+    
+    wordcloud = wc.WordCloud(font_path = '/System/Library/Fonts/HelveticaNeue.dfont', 
+        height = 400, width = 600, margin=2, background_color='white', 
+        ranks_only=None, prefer_horizontal=.9, mask=None, scale=1, color_func=None,
+        max_font_size=180, min_font_size=4, font_step=2, max_words=40, relative_scaling=0.3,
+        regexp=None, collocations=True, random_state=None, mode="RGB",
+        colormap=None, normalize_plurals=True)
+    wordcloud.generate(filepath)
+    image = wordcloud.to_image()
+
+    image.save(outputImgPath, format='png')
+    #with args.imagefile:
+    #    out = args.imagefile if sys.version < '3' else args.imagefile.buffer
+    #    image.save(outputImgPath, format='png')
+    filepath = re.sub(r'app/', '', outputImgPath)
+    return(filepath)
+
+
 
 # 'Main' page with a list of all the rooms the user is part of
 @app.route('/main')
@@ -75,7 +112,18 @@ def main():
 
     rooms = listRooms()
 
-    return render_template("main.html", key = displayName)
+    #generate wordclouds
+    path = generate_wordcloud('static/wordcloudimage.png', 'test', '../app/static/files/a_new_hope.txt')
+    path2 = generate_wordcloud('static/wordcloudimage2.png', 'test', '../app/static/files/constitution.txt')
+    path3 = generate_wordcloud('static/wordcloudimage3.png', 'test', '../app/static/files/constitution.txt')
+    path4 = generate_wordcloud('static/wordcloudimage4.png', 'test', '../app/static/files/a_new_hope.txt')
+    #####
+
+    return render_template("main.html", key = displayName, rooms=rooms,
+        imga=path,
+        imgb=path2,
+        imgc=path3,
+        imgd=path4)
 
 
 
@@ -107,6 +155,8 @@ def authorise():
 @app.route('/')
 def index():
     session.clear()
+    print("hello 2")
+
     return render_template('index.html')
 
 if __name__ == '__main__':
