232a87730b3a00f4ed24b73a7726a412c3da9913
Adrian <adrianchung@hotmail.co.uk>
2017-07-23 22:49:44 +0100

    Add files via upload

---
+++ webserver/credentials.sh
@@ -0,0 +1,5 @@
+# To run, type 'source <file name>.sh' into terminal:
+export client_id=Cc5524daf8d78d8c915794a45aa0faedc0e577f7b33ad68ef8890d89ec3a22d08
+export client_secret=c97b756d7b3c1151cacd2a922439adc58994c75478b6310823e4cc377372a848
+
+export secret_key=\x19\x90\x80\x13\xc7\n\xd12\xb3{\xc0J\xc2L\xc0\xe9\xdc\x05v\x15\xb4\xd1\x15y
