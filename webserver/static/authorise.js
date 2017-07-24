99c8081b9345d5d256804a2434949a90c47fff86
Adrian <adrianchung@hotmail.co.uk>
2017-07-24 17:20:44 +0100

    Update authorise.js

--- webserver/static/authorise.js
+++ webserver/static/authorise.js
@@ -1,16 +1,23 @@
+// Redirect to main page after a certain number of seconds
+setTimeout(function() {
+  window.location.href = "{{ url_for('main') }}";
+  // window.location.href = "../templates/main.html";
+}, 3300);
+
+// Letter reveal for the app name using GSAP Tween Lite
 window.onload = function(){
-  var tl = new TimelineLite({delay: 1, repeat: 3}),
-    firstBG = document.querySelectorAll('.letter1_bg'),
-    secondBG = document.querySelectorAll('.letter2_bg'),
-    thirdBG = document.querySelectorAll('.letter3_bg'),
-    fourthBG = document.querySelectorAll('.letter4_bg'),
-    fifthBG = document.querySelectorAll('.letter5_bg'),
-    letter1 = document.querySelectorAll('.letter1'),
-    letter2 = document.querySelectorAll('.letter2'),
-    letter3 = document.querySelectorAll('.letter3'),
-    letter4 = document.querySelectorAll('.letter4'),
-    letter5 = document.querySelectorAll('.letter5'),
-    redirect = document.querySelectorAll('.redirect');
+  var tl = new TimelineLite({delay: 1}),
+      firstBG = document.querySelectorAll('.letter1_bg'),
+      secondBG = document.querySelectorAll('.letter2_bg'),
+      thirdBG = document.querySelectorAll('.letter3_bg'),
+      fourthBG = document.querySelectorAll('.letter4_bg'),
+      fifthBG = document.querySelectorAll('.letter5_bg'),
+      letter1 = document.querySelectorAll('.letter1'),
+      letter2 = document.querySelectorAll('.letter2'),
+      letter3 = document.querySelectorAll('.letter3'),
+      letter4 = document.querySelectorAll('.letter4'),
+      letter5 = document.querySelectorAll('.letter5'),
+      redirect = document.querySelectorAll('.redirect');
 
   tl
     .to(firstBG, 0.2, {scaleX:1})
@@ -29,5 +36,4 @@ window.onload = function(){
     .to(letter5, 0.1, {opacity:1}, "-=0.1")
     .to(fifthBG, 0.2, {scaleX:0})
     .to(redirect, 0.1, {opacity:1}, "+=0.5");
-
 }
